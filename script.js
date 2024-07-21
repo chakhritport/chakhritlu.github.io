913d931e005b4ea67902dd37e6219dc8b87b93c0
Deezer1 <Deezer1.CH@gmail.com>
2024-07-22 01:22:12 +0700

    Add initial HTML file for portfolio

---
+++ script.js
@@ -0,0 +1,15 @@
+let lastScrollTop = 0;
+const header = document.querySelector('header');
+
+window.addEventListener('scroll', () => {
+    let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+
+    if (scrollTop > lastScrollTop) {
+        // ถ้า scroll down
+        header.classList.remove('show');
+    } else {
+        // ถ้า scroll up
+        header.classList.add('show');
+    }
+    lastScrollTop = scrollTop;
+});
