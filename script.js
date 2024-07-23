637c8d22506bae9d7d37b8d31fb5357eb71faee9
Deezer1 <Deezer1.CH@gmail.com>
2024-07-23 13:20:11 +0700

    Add initial HTML file for portfolio

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
 document.addEventListener("DOMContentLoaded", function() {
-    // หากมีการคลิกที่ลิงก์ในเมนู Navigation
     document.querySelectorAll('nav ul li a[href^="#"]').forEach(anchor => {
         anchor.addEventListener('click', function(e) {
             e.preventDefault();
-
             const target = document.querySelector(this.getAttribute('href'));
-
             if (target) {
-                target.scrollIntoView({
-                    behavior: 'smooth',
-                    block: 'start'
-                });
+                target.scrollIntoView({ behavior: 'smooth', block: 'start' });
+                // เพิ่ม class เพื่อเริ่มการเคลื่อนไหว
+                const textToAnimate = document.querySelector('.move-text');
+                textToAnimate.classList.add('move-text-active');
+
+                // หลังจากเวลาที่กำหนดใน animation จบ ให้ลบ class ทิ้ง
+                setTimeout(() => {
+                    textToAnimate.classList.remove('move-text-active');
+                }, 500); // 500 milliseconds ตรงกับความยาวของ animation
             }
         });
     });
